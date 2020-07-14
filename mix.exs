ea4ac436b6d4ebfdaa6b557cc5fd47b51d922322
Mateusz Front <mateusz.front@swmansion.com>
2020-07-14 09:13:17 +0200

    bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.SDL.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @github_url "https://github.com/membraneframework/membrane-element-sdl"
 
   def project do
