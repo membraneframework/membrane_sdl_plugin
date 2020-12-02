9c27f0488ceb8fd9513ef5ef20be42c0aa9022f8
Mateusz Front <mateusz.front@swmansion.com>
2020-12-02 13:18:06 +0100

    update deps

--- mix.exs
+++ mix.exs
@@ -57,8 +57,8 @@ defmodule Membrane.SDL.Plugin.MixProject do
       {:membrane_common_c, "~> 0.5.0"},
       {:membrane_caps_video_raw, "~> 0.1.0"},
       {:unifex, "~> 0.3.2"},
-      {:membrane_element_ffmpeg_h264, ">= 0.0.0", only: :test},
-      {:membrane_element_hackney, ">= 0.0.0", only: :test},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.6.0", only: :test},
+      {:membrane_hackney_plugin, "~> 0.4.0", only: :test},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false}
     ]
