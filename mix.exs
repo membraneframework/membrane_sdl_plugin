f6f1adedff0b33c61db9b3f1f47277adaea6fa18
Michał Śledź <michal.sledz@swmansion.com>
2021-06-23 13:03:53 +0200

    bump to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.SDL.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_sdl_plugin"
 
   def project do
@@ -53,7 +53,7 @@ defmodule Membrane.SDL.Plugin.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.6.0"},
+      {:membrane_core, "~> 0.7.0"},
       {:membrane_common_c, "~> 0.6.0"},
       {:membrane_caps_video_raw, "~> 0.1.0"},
       {:unifex, "~> 0.4.0"},
