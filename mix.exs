2f2a2cc03f8d4f7e822a5a72d080038ca5797c79
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-12-20 14:52:42 +0100

    Update deps

--- mix.exs
+++ mix.exs
@@ -55,13 +55,13 @@ defmodule Membrane.SDL.Plugin.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.10.0"},
-      {:membrane_common_c, "~> 0.13.0"},
+      {:membrane_core, "~> 0.11.0"},
+      {:membrane_common_c, "~> 0.14.0"},
       {:membrane_raw_video_format, "~> 0.2.0"},
       {:unifex, "~> 1.0"},
       # Testing
-      {:membrane_h264_ffmpeg_plugin, "~> 0.17", only: :test},
-      {:membrane_hackney_plugin, "~> 0.7", only: :test},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.22", only: :test},
+      {:membrane_hackney_plugin, "~> 0.9", only: :test},
       # Development
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
